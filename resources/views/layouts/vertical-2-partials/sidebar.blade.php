e0962d53354eeaa065dead7200a53e75966549cf
ilhamgusti <ilham.wibowo51@gmail.com>
2021-06-29 17:32:20 +0700

    crud blade

--- resources/views/layouts/vertical-2-partials/sidebar.blade.php
+++ resources/views/layouts/vertical-2-partials/sidebar.blade.php
@@ -1,107 +1,144 @@
       <!-- Left Sidenav -->
-        <div class="left-sidenav">           
-            <!-- LOGO -->
-            <div class="topbar-left">
-                <a href="/analytics/analytics-index" class="logo">
-                    <span>
-                        <img src="{{ URL::asset('assets/images/logo-sm.png')}}" alt="logo-small" class="logo-sm">
-                    </span>
-                    <span>
-                        <img src="{{ URL::asset('assets/images/logo.png')}}" alt="logo-large" class="logo-lg logo-light">
-                        <img src="{{ URL::asset('assets/images/logo-dark.png')}}" alt="logo-large" class="logo-lg logo-dark">
-                    </span>
-                </a>
-            </div>
-            <!--end logo--> 
-            <div class="leftbar-profile p-3 w-100">
-                <div class="media position-relative">
-                    <div class="leftbar-user online">
-                        <img src="{{ URL::asset('assets/images/users/user-9.jpg')}}" alt="" class="thumb-md rounded-circle"> 
-                    </div>                                                         
-                    <div class="media-body align-self-center text-truncate ml-3">                        
-                        <h5 class="mt-0 mb-1 font-weight-semibold">{{Auth::user()->vendor->name ?? Auth::user()->name}}</h5>   
-                        <p class="text-uppercase mb-0 font-12">{{Auth::user()->role}}</p>                                         
-                    </div><!--end media-body-->
-                </div>
-            </div>
-            <ul class="metismenu left-sidenav-menu slimscroll">
-                <li class="menu-label">Main</li>
-               
-                @yield('sidebarPortion')
-               
-                <li class="leftbar-menu-item">
-                    <a href="/home" class="menu-link">
-                        <i data-feather="grid" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>Dashboard</span>
-                        <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span>
-                    </a>
-                </li>                   
-                <li class="leftbar-menu-item">
-                    <a href="{{route('vendor.index')}}" class="menu-link">
-                        <i data-feather="grid" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>List Vendor</span>
-                        <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span>
-                    </a>
-                </li>                   
-                <li class="leftbar-menu-item">
-                    <a href="{{route('proyek.index')}}" class="menu-link">
-                        <i data-feather="grid" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>List Proyek</span>
-                        <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span>
-                    </a>
-                </li>                   
-                <li class="leftbar-menu-item">
-                    <a href="{{route('kontrak.index')}}" class="menu-link">
-                        <i data-feather="grid" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>List Kontrak</span>
-                        <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span>
-                    </a>
-                </li>                   
-                <li class="leftbar-menu-item">
-                    <a href="{{route('invoice.index')}}" class="menu-link">
-                        <i data-feather="grid" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>List Invoice</span>
-                        <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span>
-                    </a>
-                </li>                   
-                <li class="leftbar-menu-item">
-                    <a href="javascript: void(0);" class="menu-link">
-                        <i data-feather="grid" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>Approval SPV Proyek</span>
-                        <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span>
-                    </a>
-                </li>                   
-                <li class="leftbar-menu-item">
-                    <a href="javascript: void(0);" class="menu-link">
-                        <i data-feather="grid" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>App. Manager Proyek</span>
-                        <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span>
-                    </a>
-                </li>                   
-                <li class="leftbar-menu-item">
-                    <a href="javascript: void(0);" class="menu-link">
-                        <i data-feather="grid" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>App. Direktur Proyek</span>
-                        <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span>
-                    </a>
-                </li>                   
-            </ul>
-            
-        </div>
-        <!-- end left-sidenav-->
+      <div class="left-sidenav">
+          <!-- LOGO -->
+          <div class="topbar-left">
+              <a href="/analytics/analytics-index" class="logo">
+                  <span>
+                      <img src="{{ URL::asset('assets/images/logo-sm.png') }}" alt="logo-small" class="logo-sm">
+                  </span>
+                  <span>
+                      <img src="{{ URL::asset('assets/images/logo.png') }}" alt="logo-large"
+                          class="logo-lg logo-light">
+                      <img src="{{ URL::asset('assets/images/logo-dark.png') }}" alt="logo-large"
+                          class="logo-lg logo-dark">
+                  </span>
+              </a>
+          </div>
+          <!--end logo-->
+          <div class="leftbar-profile p-3 w-100">
+              <div class="media position-relative">
+                  <div class="leftbar-user online">
+                      <img src="{{ URL::asset('assets/images/users/user-9.jpg') }}" alt=""
+                          class="thumb-md rounded-circle">
+                  </div>
+                  <div class="media-body align-self-center text-truncate ml-3">
+                      <h5 class="mt-0 mb-1 font-weight-semibold">
+                          {{ Auth::user()->vendor->name ?? Auth::user()->name }}
+                      </h5>
+                      <p class="text-uppercase mb-0 font-12">{{ Auth::user()->role }}</p>
+                  </div>
+                  <!--end media-body-->
+              </div>
+          </div>
+          <ul class="metismenu left-sidenav-menu slimscroll">
+              <li class="menu-label">Main</li>
+
+              @yield('sidebarPortion')
+
+              <li class="leftbar-menu-item">
+                  <a href="/home" class="menu-link">
+                      <i data-feather="monitor" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                      <span>Dashboard</span>
+                      <span class="menu-arrow">
+                          <i class="mdi mdi-chevron-right"></i>
+                      </span>
+                  </a>
+              </li>
+              @canany(['isControlUnit', 'isVendor'])
+                  <li class="leftbar-menu-item">
+                      <a href="{{ route('proyek.index') }}" class="menu-link">
+                          <i data-feather="book-open" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                          <span>List Proyek</span>
+                          <span class="menu-arrow">
+                              <i class="mdi mdi-chevron-right"></i>
+                          </span>
+                      </a>
+                  </li>
+              @endcanany
+              @canany(['isControlUnit', 'isVendor'])
+                  <li class="leftbar-menu-item">
+                      <a href="{{ route('kontrak.index') }}" class="menu-link">
+                          <i data-feather="file-text" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                          <span>List Kontrak</span>
+                          <span class="menu-arrow">
+                              <i class="mdi mdi-chevron-right"></i>
+                          </span>
+                      </a>
+                  </li>
+              @endcanany
+              @canany(['isControlUnit'])
+                  <li class="leftbar-menu-item">
+                      <a href="{{ route('invoice.index') }}" class="menu-link">
+                          <i data-feather="inbox" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                          <span>List Invoice</span>
+                          <span class="menu-arrow">
+                              <i class="mdi mdi-chevron-right"></i>
+                          </span>
+                      </a>
+                  </li>
+              @endcanany
+              @canany(['isSPV'])
+                  <li class="leftbar-menu-item">
+                      <a href="javascript: void(0);" class="menu-link">
+                          <i data-feather="check-square"
+                              class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                          <span>Approval SPV Proyek</span>
+                          <span class="menu-arrow">
+                              <i class="mdi mdi-chevron-right"></i>
+                          </span>
+                      </a>
+                  </li>
+              @endcanany
+
+              @canany(['isManager'])
+                  <li class="leftbar-menu-item">
+                      <a href="javascript: void(0);" class="menu-link">
+                          <i data-feather="check-square"
+                              class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                          <span>App. Manager Proyek</span>
+                          <span class="menu-arrow">
+                              <i class="mdi mdi-chevron-right"></i>
+                          </span>
+                      </a>
+                  </li>
+              @endcanany
+              @canany(['isDirektur'])
+                  <li class="leftbar-menu-item">
+                      <a href="javascript: void(0);" class="menu-link">
+                          <i data-feather="check-square"
+                              class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                          <span>App. Direktur Proyek</span>
+                          <span class="menu-arrow">
+                              <i class="mdi mdi-chevron-right"></i>
+                          </span>
+                      </a>
+                  </li>
+              @endcanany
+              @canany(['isControlUnit', 'isKeuangan'])
+                  <li class="leftbar-menu-item">
+                      <a href="javascript: void(0);" class="menu-link">
+                          <i data-feather="check-square"
+                              class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                          <span>Approval. Invoice</span>
+                          <span class="menu-arrow">
+                              <i class="mdi mdi-chevron-right"></i>
+                          </span>
+                      </a>
+                  </li>
+              @endcanany
+              @canany(['isControlUnit'])
+                  <li class="leftbar-menu-item">
+                      <a href="javascript: void(0);" class="menu-link">
+                          <i data-feather="check-square"
+                              class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
+                          <span>Approval. Tahapan</span>
+                          <span class="menu-arrow">
+                              <i class="mdi mdi-chevron-right"></i>
+                          </span>
+                      </a>
+                  </li>
+              @endcanany
+          </ul>
+
+      </div>
+      <!-- end left-sidenav-->
--- resources/views/web/vendor/index.blade.php
+++ resources/views/web/vendor/index.blade.php
@@ -2,93 +2,75 @@
 
 @section('title', 'Proyek Monitoring')
 @section('headerStyle')
-     <!-- DataTables -->
-        <link href="{{ URL::asset('plugins/datatables/dataTables.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
-        <link href="{{ URL::asset('plugins/datatables/buttons.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
-        <!-- Responsive datatable examples -->
-        <link href="{{ URL::asset('plugins/datatables/responsive.bootstrap4.min.css')}}" rel="stylesheet" type="text/css" />
-@stop
-@section('sidebarPortion')
-      <li class="leftbar-menu-item">
-                    <a href="javascript: void(0);" >
-                        <i data-feather="monitor" class="align-self-center vertical-menu-icon icon-dual-vertical"></i>
-                        <span>Dashboard</span>
-                        {{-- <span class="menu-arrow">
-                            <i class="mdi mdi-chevron-right"></i>
-                        </span> --}}
-                    </a>
-                    {{-- <ul class="nav-second-level" aria-expanded="false">
-                        <li class="nav-item"><a class="nav-link" href="/vertical-2/analytics-index"><i class="ti-control-record"></i>Analytics</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/vertical-2/crypto-index"><i class="ti-control-record"></i>Crypto</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/vertical-2/crm-index"><i class="ti-control-record"></i>CRM</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/vertical-2/projects-index"><i class="ti-control-record"></i>Project</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/vertical-2/ecommerce-index"><i class="ti-control-record"></i>Ecommerce</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/vertical-2/helpdesk-index"><i class="ti-control-record"></i>Helpdesk</a></li>
-                        <li class="nav-item"><a class="nav-link" href="/vertical-2/hospital-index"><i class="ti-control-record"></i>Hospital</a></li> 
-                    </ul> --}}
-                </li>
+    <!-- DataTables -->
+    <link href="{{ URL::asset('plugins/datatables/dataTables.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <link href="{{ URL::asset('plugins/datatables/buttons.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
+    <!-- Responsive datatable examples -->
+    <link href="{{ URL::asset('plugins/datatables/responsive.bootstrap4.min.css') }}" rel="stylesheet" type="text/css" />
 @stop
 
 @section('content')
-   <div class="container-fluid">
-                    <!-- Page-Title -->
-                    <div class="row">
-                        <div class="col-sm-12">
-                             @component('common-components.breadcrumb')
-                                 @slot('title') List Vendor @endslot
-                                 @slot('item1') Home @endslot
-                                 @slot('item2')  @endslot
-                                 @endcomponent
+    <div class="container-fluid">
+        <!-- Page-Title -->
+        <div class="row">
+            <div class="col-sm-12">
+                @component('common-components.breadcrumb')
+                    @slot('title') List Vendor @endslot
+                    @slot('item1') Home @endslot
+                    @slot('item2') @endslot
+                @endcomponent
 
-                        </div><!--end col-->
+            </div>
+            <!--end col-->
+        </div>
+        <!-- end page title end breadcrumb -->
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        <table id="datatable" class="table table-bordered dt-responsive nowrap"
+                            style="border-collapse: collapse; border-spacing: 0; width: 100%;">
+                            <thead>
+                                <tr>
+                                    <th>Nama</th>
+                                    <th>Kode Vendor</th>
+                                    <th>Alamat</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($data as $item)
+                                    <tr>
+                                        <td>{{ $item->nama }}</td>
+                                        <td>{{ $item->kode_vendor }}</td>
+                                        <td>{{ $item->alamat }}</td>
+                                    </tr>
+                                @empty
+
+                                @endforelse
+
+                            </tbody>
+                        </table>
                     </div>
-                    <!-- end page title end breadcrumb -->
-                    <div class="row">
-                        <div class="col-12">
-                            <div class="card">
-                                <div class="card-body">    
-                                    <table id="datatable" class="table table-bordered dt-responsive nowrap" style="border-collapse: collapse; border-spacing: 0; width: 100%;">
-                                        <thead>
-                                        <tr>
-                                            <th>Nama</th>
-                                            <th>Kode Vendor</th>
-                                            <th>Alamat</th>
-                                        </tr>
-                                        </thead>
-                                        <tbody>
-                                            @forelse ($data as $item)
-                                            <tr>
-                                                <td>{{$item->nama}}</td>
-                                                <td>{{$item->kode_vendor}}</td>
-                                                <td>{{$item->alamat}}</td>
-                                            </tr>
-                                            @empty
-                                                
-                                            @endforelse
-
-                                        </tbody>
-                                    </table>
-                                </div>
-                            </div>
-                        </div> <!-- end col -->
-                    </div> <!-- end row -->
-                </div><!-- container -->
+                </div>
+            </div> <!-- end col -->
+        </div> <!-- end row -->
+    </div><!-- container -->
 @stop
 @section('footerScript')
-  <!-- Required datatable js -->
-         <script src="{{ URL::asset('plugins/datatables/jquery.dataTables.min.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/dataTables.bootstrap4.min.js')}}"></script>
-         <!-- Buttons examples -->
-         <script src="{{ URL::asset('plugins/datatables/dataTables.buttons.min.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/buttons.bootstrap4.min.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/jszip.min.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/pdfmake.min.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/vfs_fonts.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/buttons.html5.min.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/buttons.print.min.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/buttons.colVis.min.js')}}"></script>
-         <!-- Responsive examples -->
-         <script src="{{ URL::asset('plugins/datatables/dataTables.responsive.min.js')}}"></script>
-         <script src="{{ URL::asset('plugins/datatables/responsive.bootstrap4.min.js')}}"></script>
-         <script src="{{ URL::asset('assets/pages/jquery.datatable.init.js')}}"></script>      
+<!-- Required datatable js -->
+<script src="{{ URL::asset('plugins/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/dataTables.bootstrap4.min.js') }}"></script>
+<!-- Buttons examples -->
+<script src="{{ URL::asset('plugins/datatables/dataTables.buttons.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/buttons.bootstrap4.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/jszip.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/pdfmake.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/vfs_fonts.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/buttons.html5.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/buttons.print.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/buttons.colVis.min.js') }}"></script>
+<!-- Responsive examples -->
+<script src="{{ URL::asset('plugins/datatables/dataTables.responsive.min.js') }}"></script>
+<script src="{{ URL::asset('plugins/datatables/responsive.bootstrap4.min.js') }}"></script>
+<script src="{{ URL::asset('assets/pages/jquery.datatable.init.js') }}"></script>
 @stop
