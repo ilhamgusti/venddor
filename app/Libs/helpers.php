157cb4c2673eaf2838c98ca7953295411cc77393
Cifran Hiu <cifranhiu21@gmail.com>
2021-07-20 16:03:52 +0700

    dynamic status on List Proyek

--- app/Libs/helpers.php
+++ app/Libs/helpers.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Models\Proyek;
+use App\Models\Invoice;
+use App\Models\Kontrak;
 
 function active_class($path, $active = 'active') {
   return call_user_func_array('Request::is', (array)$path) ? $active : '';
@@ -48,22 +50,6 @@ function transformProyekStatus($status) {
     }
 }
 function transformKontrakStatus($status) {
-    // switch ($status) {
-    //     case -1:
-    //         return 'Upload Kontrak';
-    //     case 0:
-    //         return 'Checking Control Unit';
-    //     case 1:
-    //         return 'Checking SPV';
-    //     case 2:
-    //         return 'Checking Manager';
-    //     case 3:
-    //         return 'Checking Direktur';
-    //     case 4:
-    //         return 'Bisa Buat Tahapan';
-    //     default:
-    //         return 'Unknown';
-    // }
     switch ($status) {
         case -1:
             return '<span class="badge badge-outline-danger">
@@ -214,7 +200,18 @@ function getProyekDate($proyek_id) {
     }
 }
 
-function transformStatusToComponent($status) {
+function transformStatusToComponent($proyek_id, $status) {
+    $invoice = Invoice::where('proyek_id', $proyek_id)->first();
+    if (isset($invoice)) {
+        return transformInvoiceStatus($invoice->status);
+    }
+
+    $kontrak = Kontrak::where('proyek_id', $proyek_id)->first();
+    if (isset($kontrak)) {
+        return transformKontrakStatus($kontrak->status);
+    }
+
+
     switch ($status) {
         case 1:
             return '<span class="badge badge-outline-danger">
